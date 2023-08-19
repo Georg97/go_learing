679a8d97c5c78a70fd7f85b9faf56df48f8241a0
georg <georghirsch97@gmail.com>
2023-08-19 20:11:43 +0200

    channels2 pre chatgpt

--- channels/channels.go
+++ channels/channels.go
@@ -1,27 +1,45 @@
 package main
 
-import (
-	"fmt"
-	// "time"
-)
+import "fmt"
 
-func fibonacci(n int, c chan int) {
+func fibonacci(c, quit chan int) {
 	x, y := 0, 1
-	for i := 0; i < n; i++ {
-		c <- x
-		x, y = y, x+y
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
 	}
-	close(c)
+}
+func customlul(val chan int) {
+	x := 1
+	for {
+		val <- x
+		x *= 2
+	}
 }
 
 func main() {
 	c := make(chan int)
-	go fibonacci(50, c)
-    for {
-        i, going := <- c
-        if !going {
-            break
-        }
-        fmt.Println(i)
-    }
+	val := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+
+	go customlul(val)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Printf("val: %v", <-val)
+		}
+	}()
+	fibonacci(c, quit)
 }
+
