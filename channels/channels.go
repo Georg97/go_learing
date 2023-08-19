39338fa81c6a3263eff7a23946cdea3c7593f5af
georg <georghirsch97@gmail.com>
2023-08-19 20:21:13 +0200

    added sync.WaitGroup with the power of chatgpt. goroutines are quite nice

--- channels/channels.go
+++ channels/channels.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	// "time"
+)
 
 func fibonacci(c, quit chan int) {
 	x, y := 0, 1
@@ -21,11 +25,11 @@ func customlul(val chan int) {
 		x *= 2
 	}
 }
-
 func main() {
 	c := make(chan int)
 	val := make(chan int)
 	quit := make(chan int)
+
 	go func() {
 		for i := 0; i < 10; i++ {
 			fmt.Println(<-c)
@@ -33,13 +37,18 @@ func main() {
 		quit <- 0
 	}()
 
-	go customlul(val)
+    var wg sync.WaitGroup
+    wg.Add(1)
 
 	go func() {
 		for i := 0; i < 10; i++ {
-			fmt.Printf("val: %v", <-val)
+			fmt.Printf("val: %v\n", <-val) // Added a newline for clarity
 		}
+        wg.Done()
 	}()
-	fibonacci(c, quit)
-}
 
+	fibonacci(c, quit)
+    go customlul(val) // Start this goroutine first
+	// time.Sleep(50 * time.Millisecond)
+    wg.Wait()
+}
